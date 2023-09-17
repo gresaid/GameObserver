b5a37b3ffcc7eb4f8288cacb10385bc6f31a2810
PFC <gresvladimir02@gmail.com>
2023-09-17 19:50:06 +0600

    Rofl

--- app/src/main/java/com/gresaid/gameobserver/MainActivity.kt
+++ app/src/main/java/com/gresaid/gameobserver/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -54,7 +55,7 @@ fun ScreenWithImageAndDimming() {
         )
     }
 }
-
+@Preview
 @Composable
 fun ScreenWithImageAndDimmingPreview() {
     Surface(
